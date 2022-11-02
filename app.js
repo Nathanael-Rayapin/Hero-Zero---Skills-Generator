f59fcc6763a9166066fc0fd39c87e2aa262e21d8
Nathanael421 <nathanael.rayapin@gmail.com>
2022-11-02 21:33:41 +0100

    Delete clone if they already exists, some espacement design and select a item Type

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
 const app = {
+    // Re-usable Element 
+    configuration: {
+        objectView: null
+    },
+
     // Initialization
     init: function () {
         const btnElem = document.querySelector('#objectNumber + button');
@@ -7,6 +12,13 @@ const app = {
 
     // First Step - Retrieve Object Input
     retrieveObjectInput: function () {
+        /* If clones are already present, delete all */
+        if (app?.objectView && app?.objectView.querySelectorAll('.object-box').length > 0) {
+            app.objectView.querySelectorAll('.object-box').forEach((box) => {
+                box.remove();
+            })
+        }
+
         /* Retrieve and Format input value */
         const inputValue = document.getElementById('objectNumber');
         if (isNaN(inputValue.value)) {
@@ -21,8 +33,11 @@ const app = {
 
         /* Create template depending on input value */
         for (let index = 0; index < enteredValue; index++) {
-            app.createObjectBox(index+1);
+            app.createObjectBox(index + 1);
         }
+
+        /* After creating all Objects, update objects positions */
+        app.updateObjectPosition();
     },
 
     // Second Step - Create Object Box
@@ -35,8 +50,27 @@ const app = {
         contentTemplate.querySelector('.object-box').id = `id_${index}`;
 
         /* Add content to the view */
-        const objectView = document.getElementById('object-view');
-        objectView.append(contentTemplate);
+        app.objectView = document.getElementById('object-view');
+        app.objectView.append(contentTemplate);
+    },
+
+    // Third Step - Update Objects Positions
+    updateObjectPosition: function () {
+        if (!app.objectView) {
+            return;
+        }
+        const objectNumber = app.objectView.querySelectorAll('.object-box');
+        if (objectNumber.length < 6) {
+            objectNumber.forEach((box) => {
+                box.classList.add('me-3');
+            })
+            app.objectView.classList.remove('justify-content-between');
+        } else {
+            objectNumber.forEach((box) => {
+                box.classList.remove('me-3');
+            })
+            app.objectView.classList.add('justify-content-between');
+        }
     }
 }
 
